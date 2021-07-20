ab299ded3bbc24192e4f66172b7f08b314c3dfe3
류상오 <rko3507@naver.com>
2021-07-20 09:22:46 +0900

    푸쉬 확인

--- frontend/src/views/profile/MyPage.vue
+++ frontend/src/views/profile/MyPage.vue
@@ -4,13 +4,44 @@
   <div>
     <div class="wrapC">
       <h1>프로필</h1>
-      
+      <div>
+      <p>{{ profileUser.nickname }}</p>
+      <p>{{ profileUser.des }}</p>
+      </div>
+      <p>{{ profileUser.regdate }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import UserApi from '../../api/UserApi';
+
 export default {
-  
+  data: () => {
+    return {
+      profileUser: null,
+    }
+  },
+  created: function() {
+    let data = {
+      userId: this.$route.params.user_id
+    }
+    UserApi.getUser(
+      data,
+      res => {
+        console.log(res)
+        this.profileUser = res.data
+      },
+      error=>{
+        console.log(error)
+      }
+    )
+  },
+  computed: {
+    ...mapState([
+      'user',
+    ]),
+  }
 }
 </script>
