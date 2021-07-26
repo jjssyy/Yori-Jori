29ba1b9c46fbbdca99d3a381f1e32e32d65a55dd
류상오 <rko3507@naver.com>
2021-07-26 18:05:04 +0900

    0726 결산 1

---
+++ backend/src/main/java/com/web/curation/model/RecipeContent.java
@@ -0,0 +1,34 @@
+package com.web.curation.model;
+
+public class RecipeContent {
+
+	private String img;
+	private String des;
+	private String thumbnail;
+	private int recipe_idx;
+	
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getDes() {
+		return des;
+	}
+	public void setDes(String des) {
+		this.des = des;
+	}
+	public String getThumbnail() {
+		return thumbnail;
+	}
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	public int getRecipe_idx() {
+		return recipe_idx;
+	}
+	public void setRecipe_idx(int recipe_idx) {
+		this.recipe_idx = recipe_idx;
+	}
+}
