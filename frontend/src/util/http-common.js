a01444dfe9502d022e678fc8387efb2be32b0294
류상오 <rko3507@naver.com>
2021-08-05 09:28:29 +0900

    aws 연동확인중

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: 'http://I5C205.p.ssafy.io/:8080/', //백엔드 포트번호
+  baseURL: 'http://i5c205.p.ssafy.io:8080/', //백엔드 포트번호
   headers: {
     'Content-type': 'application/json',
   },
