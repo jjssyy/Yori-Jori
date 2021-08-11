eb015a50524f44b24da94259fe9818c3e8c2b3ed
류상오 <rko3507@naver.com>
2021-08-11 10:33:20 +0900

    8011-1

---
+++ backend/src/main/java/com/web/curation/model/Achievecurrent.java
@@ -0,0 +1,41 @@
+package com.web.curation.model;
+
+public class Achievecurrent {
+	
+	private String id;
+	private String title;
+	private int clear_cnt;
+	private int achieve_cnt;
+	private int percent;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getClear_cnt() {
+		return clear_cnt;
+	}
+	public void setClear_cnt(int clear_cnt) {
+		this.clear_cnt = clear_cnt;
+	}
+	public int getAchieve_cnt() {
+		return achieve_cnt;
+	}
+	public void setAchieve_cnt(int achieve_cnt) {
+		this.achieve_cnt = achieve_cnt;
+	}
+	public int getPercent() {
+		return percent;
+	}
+	public void setPercent(int percent) {
+		this.percent = percent;
+	}
+
+}
