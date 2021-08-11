641ee0c119cc49a700273a9f17db597fe02a9939
전시영 <nnaoww@naver.com>
2021-08-11 15:09:17 +0900

    복구2

--- backend/src/main/java/com/web/curation/controller/newsfeed/FeedController.java
+++ backend/src/main/java/com/web/curation/controller/newsfeed/FeedController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.web.curation.model.AchieveListFromDB;
 import com.web.curation.model.CommentFromDB;
 import com.web.curation.model.CommentToClient;
 import com.web.curation.model.FeedRecipe;
