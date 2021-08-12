d8c00d2fe2d98e7c2c4e443f1ad3d1e761048bb0
jihyeonsongb <dhqtl1@naver.com>
2021-08-12 23:19:21 +0900

    글 수정부분 해시태그, 업적 카테고리 기능 추가

--- frontend/src/views/recipe/UpdateRecipe.vue
+++ frontend/src/views/recipe/UpdateRecipe.vue
@@ -26,23 +26,37 @@
       </div>
 
       <div id="image-des">
+        <div class="hash-input-group">
+          <h2>TAGs</h2>
+          <div class="hash-input">
+            <input type="text" v-model="temphash" @keyup.enter="createHash" placeholder="태그를 입력하세요">
+            <button @click="createHash"><i class="fas fa-pen nav-icon"></i></button>
+          </div>
+        </div>
+        <div v-for="(h, idx) in recipeContent.tag" :key="idx">
+          <div class="hash">
+          <span># {{h}}</span>
+          <span @click="deleteHash(recipeContent.hashtag_idx[idx],idx)"><i id="hashDelete" class="fas fa-times"></i></span>
+          </div>
+        </div>
+      </div>
+
+      <div id="image-des">
         <h2>Challenges</h2>
         <div>
           <select name="master_name" id="master_name" v-model="masterSelected" @change="updateMaster">
-            <option value="achieve_master" selected >{{ recipeContent.achieve_master }} </option>
             <option v-for="(master_name, idx) in master_names" :key="idx" :value="master_name">
               {{ master_name }}
             </option>
           </select>
           <select name="slave_name" id="slave_name" v-model="slaveSelected">
-            <option value="" selected disabled hidden> 소분류 선택 </option>
             <option v-for="(slave_name, idx) in slave_names" :key="idx" :value="slave_name">
               {{ slave_name }}
             </option>
           </select>
         </div>
       </div>
-        <button @click="check">등록</button>
+        <button class="submit" @click="check"><h1><i class="fas fa-pen nav-icon"></i></h1></button>
       </div>
     </div>
  
@@ -64,18 +78,33 @@ export default {
       slave_names: [],
       masterSelected: '',
       slaveSelected: '',
+      deleteHashtag: [],
+      temphash:'',
     }
   },
   components:{
     UpdateForm,
   },
   methods: {
+    createHash(){
+      this.recipeContent.tag.push(this.temphash)
+      this.recipeContent.hashtag_idx.push(-1)
+      this.temphash = ''
+    },
+    deleteHash(tagidx, idx){
+      this.recipeContent.tag.splice(idx,1)
+      this.recipeContent.hashtag_idx.splice(idx,1)
+      if(tagidx!=-1){
+      this.deleteHashtag.push(tagidx)
+      }
+      console.log('dkdk')
+    },
     updateMaster(){
       this.slave_names = []
       if(this.masterSelected) {
-        for(let i = 0; i< this.Achieves.length; i++){
-          if(this.masterSelected === this.Achieves[i].achieve_master_name){
-            this.slave_names.push(this.Achieves[i].achieve_slave_name)
+        for(let achieve of this.Achieves){
+          if(this.masterSelected === achieve.achieve_master_name){
+            this.slave_names.push(achieve.achieve_slave_name)
           }
         }
       }
@@ -92,12 +121,19 @@ export default {
       var frm = new FormData();
       frm.append("recipe_idx", this.$route.params.recipe_idx)
       frm.append("title", this.title)
+      console.log(this.masterSelected)
+      frm.append('achieve_master',this.masterSelected)
+      frm.append('achieve_slave',this.slaveSelected)
       for (let i=0; i< this.recipe.length; i++){
         frm.append("content_idx["+i+"]", this.recipe[i].idx)
         frm.append("des["+i+"]",this.recipe[i].des)
         frm.append("img["+i+"]",this.recipe[i].img)
         frm.append("thumbnail["+i+"]",this.recipe[i].thumbnail)
         frm.append("content_order["+i+"]",i)
+      }
+      for (let i=0; i< this.recipeContent.tag.length; i++){
+        frm.append("hashtag_idx["+i+"]",this.recipeContent.hash_idx[i])
+        frm.append("tag["+i+"]",this.recipeConent.tag[i])
       }
       if(this.deleteContents.length == 0){
         const deleteContent = [-1]
@@ -106,6 +142,16 @@ export default {
       else {
         for(let i=0;i<this.deleteContents.length; i++){
         frm.append("deleteContents["+i+"]",this.deleteContents[i])
+        } 
+      }
+
+      if(this.deleteHashtag.length == 0){
+        this.deleteHashtag = [-1]
+        frm.append("deletehashtag[0]",this.deleteHashtag[0])
+      }
+      else {
+        for(let i=0;i<this.deleteHashtag.length; i++){
+        frm.append("deletehashtag["+i+"]",this.deleteHashtag[i])
         } 
       }
       // FormData의 key 확인
@@ -145,6 +191,8 @@ export default {
         this.recipe = res.data.recipeContent.recipe_contents
         this.recipeContent =  res.data.recipeContent
         this.title = res.data.recipeContent.title
+        this.masterSelected = res.data.recipeContent.achieve_master
+        this.slaveSelected = res.data.recipeContent.achieve_slave
       },
       error => {
         console.log(error)
@@ -157,14 +205,20 @@ export default {
         console.log(res)
         console.log("칭호 가져옴")
         this.Achieves = res.data.achieveList
-        for(let i = 0; i< this.Achieves.length; i++){
-          this.master_names.push(this.Achieves[i].achieve_master_name)
+        for(let achieve of this.Achieves){
+          this.master_names.push(achieve.achieve_master_name)
         }
         this.master_names = new Set(this.master_names)
+
+        for(let achieve of this.Achieves){
+          if(this.masterSelected === achieve.achieve_master_name){
+            this.slave_names.push(achieve.achieve_slave_name)
+          }
+        }
       },
       err=> {
         console.log(err)
-      }      
+      }
     )
   },
   computed: {
@@ -174,6 +228,70 @@ export default {
   }
 }
 </script>
-<style lang="">
-  
+<style scoped>
+#image-des{
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  padding: 2%;
+}
+
+#image-des h2{
+  width: 100%;
+}
+#image-des .hash-input-group{
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.hash-input{
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  border:1px solid #FF9636;
+  border-radius: 3px;
+}
+.hash-input input{
+  height: 70%;
+  background-color: #fafafa;
+  border: none;
+  margin-right: 2px;
+}
+.hash-input button{
+  height: 70%;
+  border-radius: 3px;
+  align-items: center;
+}
+.hash-input svg{
+  margin-right: 2px;
+}
+
+.hash{
+  background-color: #FF9636;
+  padding: 0px 10px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  color: #fff;
+  font-size: 1.2rem;
+  font-weight: 500;
+  min-width: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+}
+#hashDelete{
+  margin-top: 6px;
+  margin-left: 8px;
+  color: #fff;
+}
+.submit{
+  width: 95%;
+  display: inline;
+  background-color: #DAD870;
+  border-radius: 3px;
+  margin-bottom: 5%;
+}
 </style>
