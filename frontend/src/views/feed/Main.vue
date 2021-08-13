912c7931495595edfd58dcc4fe923079a17cd8f4
Im Gwang-hun <lgh82229047@gmail.com>
2021-08-13 13:53:15 +0900

    feeditem

--- frontend/src/views/feed/Main.vue
+++ frontend/src/views/feed/Main.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="feed newsfeed">
-    <div class="wrapB">
-      <h1>뉴스피드</h1>
-        <div v-for="(latestFeed, idx) in latestFeeds" :key="idx">
-        <LatestFeed
-          :latestFeed="latestFeed" 
-          :idx="idx"
-          :latestFeeds="latestFeeds"
-          />
-      </div>
+  <div class="newsfeed"
+  v-infinite-scroll="loadMore" 
+  infinite-scroll-disabled="busy" 
+  infinite-scroll-distance="20"
+  >
+      <div class="feed" v-for="(latestFeed, idx) in latestFeeds" :key="idx">
+      <FeedItem
+        :latestFeed="latestFeed" 
+        :idx="idx"
+        />
     </div>
   </div>
 </template>
@@ -16,43 +16,47 @@
 <script>
 import UserApi from '../../api/UserApi';
 import { mapState } from "vuex";
-// import "../../components/css/feed/feed-item.scss";
-// import "../../components/css/feed/newsfeed.scss";
-// import FeedItem from "../../components/feed/FeedItem.vue";
-import LatestFeed from "../../components/feed/LatestFeed.vue";
+import FeedItem from "../../components/feed/FeedItem.vue";
 
 export default {
   props: ["keyword"],
   data:() => {
     return {
       latestFeeds: [],
+      page : 1,
+      busy : false
     }
   },
   components: { 
-    // FeedItem,
-    LatestFeed,
-   }, 
-   methods: {
-      logout(){
-      alert('로그아웃 되었습니다.');
-      this.$store.dispatch('logout');
-      this.$router.push({name:'Login'});
-    },},
-  created: function() {
-    let data = {
-      id: this.userId
-    }
-    console.log(this.userId);
-    UserApi.latestFeed(
-      data,
-      res => {
-        console.log(res.data)
-        this.latestFeeds = res.data.latestFeed
-      },
-      error => {
-        console.log(error)
+    FeedItem,
+  }, 
+  methods: {
+    getPost(){
+      let data = {
+        id: this.userId,
+        page : this.page++
       }
-    )
+      UserApi.latestFeed(
+        data,
+        res => {
+          const items = res.data.latestFeed.map(item => {
+            return item
+          })
+          this.busy = false
+          this.latestFeeds = this.latestFeeds.concat(items)
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    },
+    loadMore(){
+      this.busy = true
+      this.getPost()
+    }
+  },
+  created: function() {
+    this.getPost()
   },
   computed: {
     ...mapState([
@@ -62,3 +66,15 @@ export default {
 }
 </script>
 
+<style scoped>
+.newsfeed{
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.feed{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
